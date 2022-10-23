0cf0aed964d659c8dfe191927abb461aa52f8eb7
liujc <jiangchuanu@163.com>
2022-10-24 07:58:23 +0800

    已开发登录页面头部管理

--- src/main/java/com/nowconder/community/service/UserService.java
+++ src/main/java/com/nowconder/community/service/UserService.java
@@ -152,6 +152,15 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket,1);
     }
 
+//    public LoginTicket findLoginTicket(String ticket){
+//        return loginTicketMapper.selectByTicket(ticket);
+//    }
+
+
+    public LoginTicket findLoginTicket(String ticket) {
+        return loginTicketMapper.selectByTicket(ticket);
+    }
+
 
 
 
